56b70eae20326c81856e34ad217c9b415969ff82
Chirag Shenoy <chiragshenoy@gmail.com>
2024-12-02 23:09:01 +0530

    Added Merged DBs

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ st.set_page_config(layout="wide")
 
 # Streamlit Sidebar for Page Navigation
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Mega List of Trades", "Performance Summary", "List of Trades", "Filtered List of Trades", "Visualization"])
+page = st.sidebar.radio("Go to", [ "Merged List of Trades" ,"Analysis of Trades", "Performance Summary", "List of Trades", "Filtered List of Trades", "Visualization"])
 
-def renderResultForScript(scrip, dataFrame, weeklyDataframe, start_date, end_date):
+def renderResultForScript(scrip, dataFrame, weeklyDataframe, start_date, end_date, show_summary_trades , show_detailed_trades):
     # Filter data for the selected scrip
         filtered_data = dataFrame[dataFrame['scrip'] == scrip]
 
@@ -49,7 +49,8 @@ def renderResultForScript(scrip, dataFrame, weeklyDataframe, start_date, end_dat
         if len(combined_trades) != 0:
 
             # Main page
-            st.subheader(f"Displaying trades for **{scrip}** from {start_date.date()} to {end_date.date()}")
+            if show_detailed_trades:
+                st.subheader(f"Displaying trades for **{scrip}** from {start_date.date()} to {end_date.date()}")
 
             # Show filtered data
             columns_to_display = ['Trade', 'Type', 'Signal', 'DateTime', 'PriceINR','DateTime_entry', 'ProfitINR', 'Profit', 'Contracts']
@@ -57,7 +58,8 @@ def renderResultForScript(scrip, dataFrame, weeklyDataframe, start_date, end_dat
             combined_trades['DateTime'] = combined_trades['DateTime'].fillna(combined_trades['DateTime_exit'])
             combined_trades = combined_trades.sort_values(by=['Trade', 'DateTime'], ascending=[False, False])
 
-            st.dataframe(combined_trades[columns_to_display])
+            if show_detailed_trades:
+                st.dataframe(combined_trades[columns_to_display])
 
             # Profit calculation
             realised_profit = 0
@@ -135,7 +137,9 @@ def renderResultForScript(scrip, dataFrame, weeklyDataframe, start_date, end_dat
                             
                             units_per_contract = math.floor(capital_per_trade / entry_price / partsOfEntry)
 
-                            st.write("Closing price: " + str(closest_close))
+                            if show_summary_trades:
+                                st.write("Closing price: " + str(closest_close))
+
                             unrealised_profit = points_captured * units_per_contract * number_of_open_contracts
 
                             # Append to results
@@ -153,11 +157,12 @@ def renderResultForScript(scrip, dataFrame, weeklyDataframe, start_date, end_dat
             results_df = pd.DataFrame(results)
 
             # Display the table and total PnL
-            st.write("### Trade Summary")
-            st.dataframe(results_df)
+            if show_summary_trades:
+                st.write("### Trade Summary")
+                st.dataframe(results_df)
 
-            st.write(f"### Realised PnL: Rs. {realised_profit:.2f}")
-            st.write(f"### Unrealised PnL: Rs. {unrealised_profit:.2f}")
+                st.write(f"### Realised PnL: Rs. {realised_profit:.2f}")
+                st.write(f"### Unrealised PnL: Rs. {unrealised_profit:.2f}")
 
             # Close the database connection
             perfConnection.close()
@@ -170,7 +175,9 @@ def renderResultForScript(scrip, dataFrame, weeklyDataframe, start_date, end_dat
             return (realised_profit, unrealised_profit)
 
         else:
-            st.write("No Trades found for " + scrip)
+            if show_summary_trades:
+                st.write("No Trades found for " + scrip)
+
             return (0,0)
 
 def get_closest_close(df, scrip_name, input_time):
@@ -200,13 +207,13 @@ def get_closest_close(df, scrip_name, input_time):
     
     # Check if the filtered DataFrame is empty
     if time_filtered_df.empty:
-        return None  # Return None if no matching rows found
+        return 0  # Return None if no matching rows found
     
     # Find the row with the maximum time (nearest to the input time)
     closest_row = time_filtered_df.loc[time_filtered_df['time'].idxmax()]
     
     # Return the closing price
-    return closest_row['close']
+    return round(float(closest_row['close']), 2)
 
 def calculate_and_display_all_metrics(df, filter_column, target_column):
     """
@@ -243,41 +250,278 @@ def calculate_and_display_all_metrics(df, filter_column, target_column):
     metrics_df = pd.DataFrame(metrics_data)
 
     st.markdown("## 📋 Consolidated Metrics Table", unsafe_allow_html=True)
-    
-    # Apply custom CSS to enlarge the table and enhance readability
-    st.markdown("""
-        <style>
-            .dataframe {
-                font-size: 18px;  /* Increase font size */
-                border-collapse: collapse;
-                width: 100%;
-                height: auto;
-            }
-            .dataframe th, .dataframe td {
-                padding: 12px;  /* Increase padding */
-                text-align: center;
-            }
-            .dataframe th {
-                background-color: #f2f2f2;  /* Header background */
-            }
-            .dataframe tr:nth-child(even) {
-                background-color: #f9f9f9;  /* Alternating row colors */
-            }
-            .dataframe tr:hover {
-                background-color: #f1f1f1;  /* Hover effect */
-            }
-        </style>
-    """, unsafe_allow_html=True)
 
     # Display the dataframe with larger font and styled table
-    st.dataframe(metrics_df.style.format(precision=2), use_container_width=True)
+    st.dataframe(metrics_df.style.format(precision=2), use_container_width=True, height = 1000)
 
-if page == "Mega List of Trades":
-    st.title("Mega list of Trades")
+def renderResultsForMergedScript(scrip, dataFrame, weeklyDataframe, start_date, end_date, show_summary_trades , show_detailed_trades):
+    
+    initial_capital = 100000
+    profitBookingCount = 3
+    
+    # Filter data for the specific scrip
+    filtered_data = dataFrame[dataFrame['scrip'] == scrip]
+    filtered_data = filtered_data.drop(columns=['drawdown_inr', 'cum_profit', 'cum_profit_inr', 'runup', 'runup_inr',
+                                                'drawdown', 'scrip', 'price_inr', 'points_captured'])
+
+    # Convert start_date and end_date to Timestamps
+    start_date = pd.Timestamp(start_date)
+    end_date = pd.Timestamp(end_date)
+
+    # Ensure datetime columns are properly converted
+    filtered_data.loc[:, 'buy_datetime'] = pd.to_datetime(filtered_data['buy_datetime'], errors='coerce')
+    filtered_data.loc[:, 'sell_datetime'] = pd.to_datetime(filtered_data['sell_datetime'], errors='coerce')
+
+    # Filter data within the date range or open trades
+    filtered_data = filtered_data[
+        (filtered_data['buy_datetime'] >= start_date) &
+        ((filtered_data['sell_datetime'] <= end_date) | filtered_data['sell_datetime'].isna())
+    ]
+
+    # Process each trade
+    for index, row in filtered_data.iterrows():
+        
+        # Case of open trade
+        if math.isnan(row['contracts']):
+            # Find the latest trade and calculate matching trades
+            latest_trade = filtered_data.loc[filtered_data['trade_id'].idxmax()]
+            latest_buy_price = latest_trade['buy_price']
+            latest_buy_datetime = latest_trade['buy_datetime']
+
+            matching_trades = len(filtered_data[
+                (filtered_data['buy_price'] == latest_buy_price) &
+                (filtered_data['buy_datetime'] == latest_buy_datetime)
+            ])
+
+            # Determine the number of open contracts
+            if matching_trades == profitBookingCount:
+                number_of_open_contracts = 1
+            elif matching_trades == 2:
+                number_of_open_contracts = 2
+            else:
+                number_of_open_contracts = profitBookingCount
+
+            # Error handling
+            if math.isnan(number_of_open_contracts):
+                number_of_open_contracts = 1
+            if math.isnan(latest_buy_price):
+                latest_buy_price = 1
+
+            units_per_contract = math.floor(initial_capital / latest_buy_price / profitBookingCount)
+
+            if math.isnan(units_per_contract):
+                units_per_contract = 1
+            
+            filtered_data.loc[index, 'units_traded'] = units_per_contract * number_of_open_contracts
+
+        else:
+            # Calculate units traded for specified contracts
+            buy_price = row['buy_price']
+            sell_price = row['sell_price']
+
+            if not math.isnan(row['buy_price']):
+                units_purchased = math.floor(initial_capital / row['buy_price'] / (profitBookingCount / row['contracts']))
+            else:
+                units_purchased = 0
+
+            filtered_data.loc[index, 'units_traded'] = units_purchased
+
+            if not math.isnan(row['buy_price']) and not math.isnan(row['sell_price']):
+                filtered_data.loc[index, 'points_realised'] = sell_price - buy_price
+                filtered_data.loc[index, 'pnl'] = (sell_price - buy_price) * units_purchased
+
+    st.subheader(f"Displaying trades for **{scrip}** from {start_date.date()} to {end_date.date()}")
+
+    # Sort and display data
+    filtered_data = filtered_data.sort_values(by=['trade_id'], ascending=[False])
+    st.dataframe(filtered_data)
+
+    # Separate closed and open trades
+    closed_trades = filtered_data[filtered_data['sell_datetime'].notna()]
+    open_trades = filtered_data[filtered_data['sell_datetime'].isna()]
+
+    open_trades = open_trades.copy()
+    closed_trades = closed_trades.copy()
+
+    closest_close = round(get_closest_close(weeklyDataframe, scrip, end_date), 2)
+    st.write("Closing Price: " + str(closest_close))
+
+    # Calculate realized metrics
+    if len(closed_trades) > 0:
+
+        realized_profit_loss = closed_trades.apply(
+            lambda row: round(row['sell_price'] * row['units_traded'] if not math.isnan(row['units_traded']) else 1, 2), axis=1
+        )
+
+        # Assign the computed values to the DataFrame
+        closed_trades.loc[:, 'realized_profit_loss'] = realized_profit_loss
+
+        total_realized_profit_loss = 0
+
+        if 'pnl' in closed_trades.columns:
+            total_realized_profit_loss = closed_trades['pnl'].sum()
+
+        realized_metrics = {
+            'total_realized_profit_loss': total_realized_profit_loss,
+            'total_closed_trades': len(closed_trades),
+            'profitable_closed_trades': len(closed_trades[closed_trades['realized_profit_loss'] > 0]),
+            'unprofitable_closed_trades': len(closed_trades[closed_trades['realized_profit_loss'] <= 0]),
+            'win_rate': len(closed_trades[closed_trades['realized_profit_loss'] > 0]) / len(closed_trades) * 100
+        }
+    else:
+        realized_metrics = {
+            'total_realized_profit_loss': 0,
+            'total_closed_trades': 0,
+            'profitable_closed_trades': 0,
+            'unprofitable_closed_trades': 0,
+            'win_rate': 0
+        }
+
+    # Calculate unrealized metrics
+    if len(open_trades) > 0:
+        open_trades.loc[:, 'current_market_price'] = closest_close
+        
+        # Compute the 'unrealized_profit_loss' values first
+        unrealized_profit_loss = open_trades.apply(
+            lambda row: round((closest_close - row['buy_price']) * row['units_traded'], 2), axis=1
+        )
+
+        # Assign the computed values to the DataFrame
+        open_trades.loc[:, 'unrealized_profit_loss'] = unrealized_profit_loss
+        
+        unrealized_metrics = {
+            'total_unrealized_profit_loss': open_trades['unrealized_profit_loss'].sum(),
+            'total_open_trades': len(open_trades)
+        }
+    else:
+        unrealized_metrics = {
+            'total_unrealized_profit_loss': 0,
+            'total_open_trades': 0
+        }
+
+    # Portfolio summary
+    portfolio_summary = {
+        'scrip': scrip,
+        'total_realized_profit_loss': round(realized_metrics['total_realized_profit_loss'], 2),
+        'total_unrealized_profit_loss': round(unrealized_metrics['total_unrealized_profit_loss'], 2),
+        'total_profit_loss': round(realized_metrics['total_realized_profit_loss'] + unrealized_metrics['total_unrealized_profit_loss'], 2),
+        'current_portfolio_value': round(initial_capital + realized_metrics['total_realized_profit_loss'] + unrealized_metrics['total_unrealized_profit_loss'], 2)
+    }
+
+    # Detailed trade analysis
+    trade_analysis = {
+        'closed_trades': {
+            'total_count': realized_metrics['total_closed_trades'],
+            'profitable_count': realized_metrics['profitable_closed_trades'],
+            'unprofitable_count': realized_metrics['unprofitable_closed_trades'],
+            'win_rate': realized_metrics['win_rate']
+        },
+        'open_trades': {
+            'total_count': unrealized_metrics['total_open_trades']
+        }
+    }
+
+    return {
+        'portfolio_summary': portfolio_summary,
+        'trade_analysis': trade_analysis
+    }
+
+
+if page == "Merged List of Trades":
+    st.title("Merged List of Trades")
     st.markdown("""
     This page allows you to analyze scrip data from the SQLite database. You can:
-    - See all trades for a given time period
-    - Get overall trades
+    - Choose a time period
+    - Get the overall PnL status of the strategy
+    - Rs.1,00,000 is the capital for each trade
+    """)
+
+    db_folder_path = "data/lot"
+
+    db_files = [f for f in os.listdir(db_folder_path) if f.endswith('.db') & f.startswith('merged') ]
+
+    show_header = st.radio( "Do you want to display the header?",("Yes", "No"))
+
+    # Dropdown to select the .db file from the folder
+    if db_files:
+        db_file_name = st.selectbox("Select a .db file", db_files)
+        
+        # Construct the full path for the selected file
+        db_file_path = os.path.join(db_folder_path, db_file_name)
+        
+        # Load and process the selected database
+        perfConnection = sqlite3.connect(db_file_path)
+
+        weeklyConnection = sqlite3.connect("data/closing/weekly_closing.db")
+        weeklyDataframe = pd.read_sql_query("SELECT * from stock_data", weeklyConnection)
+
+        # Query the data from the selected database
+        dataFrame = pd.read_sql_query("SELECT * FROM table_name", perfConnection)
+        
+        st.sidebar.title("Filter Options")
+
+        # Select scrip
+        scrips = dataFrame['scrip'].unique()
+
+        start_date = st.sidebar.date_input("Start Date", value=pd.Timestamp("2020-01-01").date())
+        end_date = st.sidebar.date_input("End Date", value=pd.Timestamp("2024-12-12").date())
+
+        total_realised_pnl = 0
+        total_unrealised_pnl = 0
+
+        for scrip in scrips:
+            
+            analysis_results = renderResultsForMergedScript(scrip, dataFrame, weeklyDataframe, start_date,
+                                                                end_date, show_summary_trades = False, show_detailed_trades= False)
+            
+            # for key, value in analysis_results['portfolio_summary'].items():
+            #     st.write(f"{key}: {value}")
+
+            # for key, value in analysis_results['trade_analysis'].items():
+            #     st.write(f"{key}: {value}")
+
+            portfolio_summary = analysis_results['portfolio_summary']
+            
+            total_realised_pnl += analysis_results['portfolio_summary']['total_realized_profit_loss']
+            total_unrealised_pnl += analysis_results['portfolio_summary']['total_unrealized_profit_loss']
+
+            # trade_analysis = analysis_results['trade_analysis']
+     
+            # Prepare a flattened table for Portfolio Summary
+            portfolio_summary_df = pd.DataFrame.from_dict([portfolio_summary])
+
+            # Prepare a flattened table for Trade Analysis
+
+            # trade_analysis_data = {
+            #     'Metric': ['Total Closed Trades', 'Profitable Trades', 'Unprofitable Trades', 'Win Rate', 'Open Trades'],
+            #     'Value': [
+            #         trade_analysis['closed_trades']['total_count'],
+            #         trade_analysis['closed_trades']['profitable_count'],
+            #         trade_analysis['closed_trades']['unprofitable_count'],
+            #         f"{trade_analysis['closed_trades']['win_rate']}%",
+            #         trade_analysis['open_trades']['total_count']
+            #     ]
+            # }
+
+            # Display the tables in Streamlit
+            st.subheader("Trades Summary")
+            st.table(portfolio_summary_df)
+            st.write("----")
+
+            # trade_analysis_df = pd.DataFrame(trade_analysis_data)
+            # st.subheader("Trade Analysis")
+            # st.table(trade_analysis_df)
+        
+        st.header("Total Realised PnL: " + str(total_realised_pnl))
+        st.header("Total Unealised PnL: " + str(total_unrealised_pnl))
+
+
+if page == "Analysis of Trades":
+    st.title("Analysis of Trades")
+    st.markdown("""
+    This page allows you to analyze scrip data from the SQLite database. You can:
+    - Choose a time period
+    - Get the overall PnL status of the strategy
     """)
 
     db_folder_path = "data/lot"
@@ -312,13 +556,13 @@ if page == "Mega List of Trades":
         total_unrealised_pnl = 0
 
         for scrip in scrips:
-            realised_pnl, unrealise_pnl = renderResultForScript(scrip, dataFrame, weeklyDataframe, start_date, end_date)
+            realised_pnl, unrealise_pnl = renderResultForScript(scrip, dataFrame, weeklyDataframe, start_date,
+                                                                end_date,show_summary_trades = False, show_detailed_trades= False)
             total_realised_pnl += realised_pnl
             total_unrealised_pnl += unrealise_pnl
-            st.write("----")
 
-        st.header("Total realised: " + str(total_realised_pnl))
-        st.header("Total unrealised: " + str(total_unrealised_pnl))
+        st.header("Total realised PnL: " + str(total_realised_pnl))
+        st.header("Total unrealised PnL: " + str(total_unrealised_pnl))
 
 
 # List of Trades Page
@@ -398,10 +642,10 @@ elif page == "Filtered List of Trades":
         selected_scrip = st.sidebar.selectbox("Select Scrip", scrips)
 
         # Date range picker
-        start_date = st.sidebar.date_input("Start Date", value=pd.Timestamp("2019-01-01").date())
-        end_date = st.sidebar.date_input("End Date", value=pd.Timestamp("2020-04-04").date())
+        start_date = st.sidebar.date_input("Start Date", value=pd.Timestamp("2020-01-01").date())
+        end_date = st.sidebar.date_input("End Date", value=pd.Timestamp("2024-12-12").date())
 
-        renderResultForScript(selected_scrip, dataFrame, weeklyDataframe, start_date, end_date)
+        renderResultForScript(selected_scrip, dataFrame, weeklyDataframe, start_date, end_date, show_summary_trades = True, show_detailed_trades = True)
         
 
 # Visualization Page
@@ -475,8 +719,6 @@ elif page == "Performance Summary":
 
         # Close the database connection
         perfConnection.close()
-
-        
 
     else:
         st.write("No .db files found in the folder.")
